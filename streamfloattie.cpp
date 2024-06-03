9c27b3a39de17bbb0d67300b6c9a768393cf19d9
rokicki <rokicki@gmail.com>
2024-06-03 14:09:08 -0700

    Add C++ streams to floats with tie.

---
+++ streamfloattie.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+using ll = long long ;
+int main(int argc, char *argv[]) {
+   ios_base::sync_with_stdio(false) ;
+   cin.tie(NULL) ;
+   ll s = 0 ;
+   double v ;
+   while (cin >> v)
+      s += v ;
+   cout << s << endl ;
+}
