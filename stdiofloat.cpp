e24c838952f9f80b711881bbd98a57896fe75228
rokicki <rokicki@gmail.com>
2024-06-03 14:21:02 -0700

    Use proper format for stdio.

--- stdiofloat.cpp
+++ stdiofloat.cpp
@@ -4,7 +4,7 @@ using ll = long long ;
 int main(int argc, char *argv[]) {
    ll s = 0 ;
    double v ;
-   while (scanf("%g", &v) == 1)
+   while (scanf("%lg", &v) == 1)
       s += v ;
    printf("%lld\n", s) ;
 }
